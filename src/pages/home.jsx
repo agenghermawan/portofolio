ac55f615ef7a8d3c46586e38b9af54d98c9145c1
agenghermawan <agenghermawan16@gmail.com>
2022-10-22 08:14:51 +0700

    Fix Aos

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Hero from "../components/hero";
 import Layout from "../components/layout";
 import AboutMe from "../components/about-me";
 import Experiences from "../components/experiences";
 import Project from "../components/project";
 import ContactMe from "../components/contact-me";
+import aos from "aos";
+import "aos/dist/aos.css";
 
 export default function Home() {
+  useEffect(() => {
+    aos.init();
+  });
   return (
     <Layout>
       <Hero />
